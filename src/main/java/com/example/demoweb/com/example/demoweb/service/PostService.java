e1d680f120bee201d5e704b191a93131febdf090
Matvey <veleskg@mail.ru>
2023-02-17 14:15:28 +0500

    Bootstrap

--- src/main/java/com/example/demoweb/com/example/demoweb/service/PostService.java
+++ src/main/java/com/example/demoweb/com/example/demoweb/service/PostService.java
@@ -4,11 +4,13 @@ import com.example.demoweb.model.Post;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Service
 public class PostService {
     public ArrayList<Post> listAllPosts(){
+        Date date = new Date();
 
         ArrayList<Post> posts = new ArrayList<>();
         posts.add(new Post("post1"));
--- src/main/java/com/example/demoweb/model/Post.java
+++ src/main/java/com/example/demoweb/model/Post.java
@@ -1,14 +1,27 @@
 package com.example.demoweb.model;
 
+import java.util.Date;
+
 public class Post {
 
+    public Post(String text, Date creationDate) {
+        this.text = text;
+        this.creationDate = creationDate;
+    }
     public Post(String text) {
         this.text = text;
+        creationDate = new Date();
     }
 
     private String text;
 
     private Integer likes;
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    private Date creationDate;
 
 
     public Integer getLikes() {
