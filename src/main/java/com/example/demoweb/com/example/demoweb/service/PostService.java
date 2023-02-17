6e22544526673bb9d80004677467f29aec33cc27
Matvey <veleskg@mail.ru>
2023-02-17 15:27:06 +0500

    CreateNewPost

--- src/main/java/com/example/demoweb/com/example/demoweb/service/PostService.java
+++ src/main/java/com/example/demoweb/com/example/demoweb/service/PostService.java
@@ -9,15 +9,12 @@ import java.util.List;
 
 @Service
 public class PostService {
+    ArrayList<Post> posts = new ArrayList<>();
     public ArrayList<Post> listAllPosts(){
-        Date date = new Date();
-
-        ArrayList<Post> posts = new ArrayList<>();
-        posts.add(new Post("post1"));
-        posts.add(new Post("post2"));
-        posts.add(new Post("post3"));
         return posts;
-
+    }
+    public void create(String text) {
+        posts.add(new Post(text, new Date()));
     }
 
 }
